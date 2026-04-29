b695576135995e4ce4c5a1c32fd9c7e41fd899c5
Denilson Santos <denilson_n.santos@hotmail.com>
2026-04-29 18:36:42 -0300

    feat: mudando classe para generics

--- generics/src/ListaSemGenerics.java
+++ generics/src/ListaSemGenerics.java
@@ -1,31 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class Lista {
-    private List<Object> lista = new ArrayList<>();
+class Lista<T> {
+    private List<T> lista = new ArrayList<>();
 
-    public void adicionar(Object elemento) {
+    public void adicionar(T elemento) {
         lista.add(elemento);
     }
 
-    public Object obter(int indice) {
+    public T obter(int indice) {
         return lista.get(indice);
     }
 }
 
 public class ListaSemGenerics {
     public static void main(String[] args) {
-        // Exemplo sem o uso de generics
-        Lista numeros = new Lista();
+        Lista<Integer> numeros = new Lista<>();
         numeros.adicionar(1);
         numeros.adicionar(2);
-        Lista textos = new Lista();
+        Lista<String> textos = new Lista<>();
         textos.adicionar("Olá");
         textos.adicionar("Mundo");
 
-        int numero = (int) numeros.obter(0); // ok, pois o tipo é int
+        int numero = numeros.obter(0);
         System.out.println(numero);
-        //String texto = (String) numeros.obter(0); // ClassCastException
-        //System.out.println(texto);
+        String texto = textos.obter(0);
+        System.out.println(texto);
     }
 }
